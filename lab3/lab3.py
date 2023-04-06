8a22961554db51f45e42f2ebf35126a965554245
Steven <skrobeson@gmail.com>
2023-04-06 11:54:01 -0400

    Read files and convert images

---
+++ lab3/lab3.py
@@ -0,0 +1,30 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras import preprocessing
+import pandas as pd
+# from PIL import Image
+from keras.utils import load_img
+from keras.utils import img_to_array
+# I don't think we need this function
+# from keras.utils import array_to_img
+
+
+training_csv = pd.read_csv('fairface_label_train.csv')
+training_csv = training_csv.iloc[1:25]
+training_csv = training_csv.reset_index()
+training_images = []
+for index, row in training_csv.iterrows():
+    image = load_img(row['file'], color_mode="grayscale", target_size=(32,32))
+    print(f"Loaded image: {type(image)}")
+    #print(f"image #{index} file name: {row['file']}")
+    #print(f"image #{index} format: {image.format}")
+    #print(f"image #{index} size: {image.size}")
+    image_array = img_to_array(image)
+    print(f"Numpy array: {type(image_array)}") 
+    print(f"image array #{index} type: {image_array.dtype}")
+    print(f"image array #{index} shape: {image_array.shape}")
+
+
