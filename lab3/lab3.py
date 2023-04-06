00b703b6f132fb2c95acb666c06e8db87f2d7e65
Steven <skrobeson@gmail.com>
2023-04-06 12:04:30 -0400

    Used Pillow instead

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -5,7 +5,7 @@ from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras import preprocessing
 import pandas as pd
-# from PIL import Image
+from PIL import Image
 from keras.utils import load_img
 from keras.utils import img_to_array
 # I don't think we need this function
@@ -17,12 +17,12 @@ training_csv = training_csv.iloc[1:25]
 training_csv = training_csv.reset_index()
 training_images = []
 for index, row in training_csv.iterrows():
-    image = load_img(row['file'], color_mode="grayscale", target_size=(32,32))
+    image = Image.open(row['file']).convert("L") # load_img(row['file'], color_mode="grayscale", target_size=(32,32))
     print(f"Loaded image: {type(image)}")
-    #print(f"image #{index} file name: {row['file']}")
-    #print(f"image #{index} format: {image.format}")
-    #print(f"image #{index} size: {image.size}")
-    image_array = img_to_array(image)
+    print(f"image #{index} file name: {row['file']}")
+    print(f"image #{index} format: {image.format}")
+    print(f"image #{index} size: {image.size}")
+    image_array = np.asarray(image) # img_to_array(image) (using the keras methods gives float32 type instead of uint8, not sure which we need)
     print(f"Numpy array: {type(image_array)}") 
     print(f"image array #{index} type: {image_array.dtype}")
     print(f"image array #{index} shape: {image_array.shape}")
